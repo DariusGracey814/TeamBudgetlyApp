d788f086fa4b2d4eac6b7e59e1afa4fe71c0257f
DariusGracey814 <Dariusgracey814@gmail.com>
2023-05-02 13:52:36 -0400

    maven install

--- application/src/main/java/com/budgetly/application/Application.java
+++ application/src/main/java/com/budgetly/application/Application.java
@@ -3,12 +3,19 @@ package com.budgetly.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class Application extends SpringBootServletInitializer {
-
+public class Application  {
+	
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(Application.class);
+	// extends SpringBootServletInitializer
+//	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
